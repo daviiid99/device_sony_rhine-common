75cf234d7df3a733e259ef2ccf0d5b9f96298551
David <davidmolinamorales1999@gmail.com>
2021-04-13 22:47:08 +0200

    Update rhine.mk

--- rhine.mk
+++ rhine.mk
@@ -161,7 +161,11 @@ PRODUCT_PACKAGES += \
     Tag
 
 # NFCEE access control
-NFCEE_ACCESS_PATH := $(COMMON_PATH)/rootdir/system/etc/nfcee_access_debug.xml
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := $(COMMON_PATH)/rootdir/system/etc/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := $(COMMON_PATH)/rootdir/system/etc/nfcee_access_debug.xml
+endif
 
 PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):$(TARGET_COPY_OUT_VENDOR)/etc/nfcee_access.xml
